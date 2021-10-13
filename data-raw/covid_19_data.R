c1261d5ac4b9f43891efbe77e8ef7140b4ac4a3c
Jørn I. Halvorsen <jorn.halvorsen@gmail.com>
2021-10-13 16:37:54 +0200

    updpres

--- data-raw/covid_19_data.R
+++ data-raw/covid_19_data.R
@@ -1,4 +1,5 @@
 ## code to prepare `DATASET` dataset goes here
+
 library(COVID19)
 library(dplyr)
 library(lubridate)
@@ -7,7 +8,6 @@ library(zoo)
 all_data <- COVID19::covid19(verbose = F)
 unique(all_data$id)
 
-# Countries to be included in the dataset
 sel_cou <- c('NOR','ITA','SWE','GBR','ISR','FIN','CZE','ESP','USA','CAN','SVK','IND')
 
 COVID19 <- all_data %>% dplyr::filter(id%in%sel_cou) %>%
@@ -15,10 +15,7 @@ COVID19 <- all_data %>% dplyr::filter(id%in%sel_cou) %>%
 dplyr::mutate(year=as.factor(lubridate::year(date))) %>%
 dplyr::mutate(dayofyear=lubridate::yday(date)) %>%
 dplyr::select(id,date,confirmed,deaths,hosp,dayofyear,year,population) %>%
-# Deaths per day
 dplyr::mutate(c_deaths=deaths-dplyr::lag(deaths)) %>%
-# Deaths moving average (7-days)
 dplyr::mutate(ma_deaths=round(rollmean(c_deaths,k=7, fill=NA)),digits=4) %>%
-#  Deaths moving average (7-days) per 100k
 dplyr::mutate(ma_deaths_perc=(ma_deaths/population)*100000) 
 usethis::use_data(COVID19, overwrite = TRUE)
