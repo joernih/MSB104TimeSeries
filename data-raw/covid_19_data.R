591b38115b4be63a5f42dee08bdfb04b2b4160d1
Jørn I. Halvorsen <jorn.halvorsen@gmail.com>
2021-11-03 11:41:29 +0100

    updatedschedule

--- data-raw/covid_19_data.R
+++ data-raw/covid_19_data.R
@@ -7,7 +7,7 @@ library(zoo)
 all_data <- COVID19::covid19(verbose = F)
 unique(all_data$id)
 
-sel_cou <- c('NOR','ITA','SWE','GBR','ISR','FIN','CZE','ESP','USA','CAN','SVK','IND')
+sel_cou <- c('NOR','ITA','SWE','GBR','ISR','FIN','CZE','ESP','USA','CAN','SVK','IND')[1]
 
 COVID19 <- all_data %>% dplyr::filter(id%in%sel_cou) %>%
 # Datering
@@ -16,5 +16,7 @@ dplyr::mutate(dayofyear=lubridate::yday(date)) %>%
 dplyr::select(id,date,confirmed,deaths,hosp,dayofyear,year,population) %>%
 dplyr::mutate(c_deaths=deaths-dplyr::lag(deaths)) %>%
 dplyr::mutate(ma_deaths=round(rollmean(c_deaths,k=7, fill=NA)),digits=4) %>%
-dplyr::mutate(ma_deaths_perc=(ma_deaths/population)*100000) 
+dplyr::mutate(ma_deaths_perc=(ma_deaths/population)*100000)
 usethis::use_data(COVID19, overwrite = TRUE)
+
+View(COVID19)
--- index.md
+++ index.md
@@ -5,9 +5,11 @@ Econometrics (MSB104) Part II: Time Series Analysis
 
 ### Schedule
 
--   Module I (week 44: Zoom/HGSD1077) Wed. 3. november: 11:15-14:00
-    Fri. 5. november: 08:15-12:00
--   Module II: (week ~~46~~ 47: Zoom/HGSD1077) Wed. ~~17.~~:24 november:
-    11:15-14:00 Fri. ~~19.~~:27 november: 08:15-12:00
+-   Module I (week 44: Zoom/HGSD1077)
+    -   Wed. 3. november: 12:15-16:00
+    -   Fri. 5. november: 10:15-14:00
+-   Module II: (week ~~46~~ 47: Zoom/HGSD1077)
+    -   Wed. ~~17.~~:24 november: 12:15-16:00
+    -   Fri. ~~19.~~:27 november: 10:15-14:00
 
-**Siste gang oppdatert: 2021-11-02**
+**Siste gang oppdatert: 2021-11-03**
